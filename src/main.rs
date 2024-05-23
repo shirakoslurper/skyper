aa21ab04d3a30e0e181dad35a1794d04ce47b70a
bonflower <54567022+asaphbay@users.noreply.github.com>
2024-05-23 14:15:29 -0700

    updated gitignore + stream provenance

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,42 @@
 use ethers::prelude::*;
-use futures::{stream, StreamExt, TryStreamExt};
+use ethers::{
+    core::{types::TransactionRequest, utils::Anvil},
+    middleware::SignerMiddleware,
+    providers::{Http, Middleware, Provider},
+    signers::{LocalWallet, Signer},
+};
+use futures::{stream, Stream, StreamExt, TryStream, TryStreamExt};
 use std::collections::HashSet;
-
 
 // const JSON_RPC_URL: &str = "https://mainnet.sanko.xyz";
 const WS_RPC_URL: &str = "wss://mainnet.sanko.xyz/ws";
 const CAMELOT_POOL_FACTORY_ADDRESS: &str = "0x5c69bee701ef814a2b6a3edd4b1652cb9cc5aa6f";
 const WDMT_ADDRESS: &str = "0x754cDAd6f5821077d6915004Be2cE05f93d176f8";
 
+#[derive(Clone, Debug)]
+enum EventType {
+    PairCreated(Log),
+    Mint(Log)
+}
+
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
+
+    // WALLET
+    let anvil = Anvil::new().spawn();
+    let wallet: LocalWallet = anvil.keys()[0].clone().into();
+
+    // CONNECT TO NETWORKS
     let provider = Provider::<Ws>::connect(WS_RPC_URL).await?;
 
+    // // CONNECT WALLET TO PROVIDER
+    // let client = SignerMiddleware::new(provider, wallet.wit)
+
+    // QUICK BLOCK NUMBER CHECK
     let block_number: U64 = provider.get_block_number().await?;
     println!("{block_number}");
 
+    // PAIRCREATED AND MINT FILTERS
     let camelot_pool_factory_address = CAMELOT_POOL_FACTORY_ADDRESS.parse::<H160>()?;
 
     let token_topics = [
@@ -30,66 +52,48 @@ async fn main() -> eyre::Result<()> {
     let mint_filter = Filter::new()
         .event("Mint(address,uint256,uint256)");
 
-    let pair_created_stream = provider.subscribe_logs(&pair_created_filter).await?;
-    let mint_stream = provider.subscribe_logs(&mint_filter).await?;
-
-    // We may want to skip combining and have these two in different processes as
-    // it's a bit of a pain to tell these events apart
-    // Simplest hack would be the contract address tho
+    // PAIRCREATED AND MINT EVENT STREAMS
+    let pair_created_stream = provider.subscribe_logs(&pair_created_filter).await?.map(Box::new(|log| EventType::PairCreated(log)) as Box<dyn Fn(Log) -> EventType>);
+    let mint_stream = provider.subscribe_logs(&mint_filter).await?.map(Box::new(|log| EventType::Mint(log)) as Box<dyn Fn(Log) -> EventType>);
 
     let mut combined_stream = stream::select_all(vec![
         pair_created_stream,
         mint_stream,
     ]);
 
+
+    // PAIR SET (REMOVED UPON FIRST LIQUIDITY)
     let mut pair_address_set = HashSet::new();
 
-    while let Some(log) = combined_stream.next().await {
-        println!("{:#?}", log);
-        if camelot_pool_factory_address == log.address {
-            let pair_address = Address::from(&log.data[40..60].try_into()?);
-            println!("PairCreated:\n    pair_address: {}", pair_address);
-            pair_address_set.insert(pair_address);
-        } else {
-            let pair_address = log.address;
+    // EVENT HANDLING LOOP
+    while let Some(event) = combined_stream.next().await {
+        println!("{:#?}", event);
 
-            println!("Mint\n    pair_address: {}", pair_address);
+        match event {
+            EventType::PairCreated(log) => {
+                let pair_address = Address::from(&log.data[40..60].try_into()?);
+                println!("PairCreated:\n    pair_address: {}", pair_address);
+                pair_address_set.insert(pair_address);
+            },
+            EventType::Mint(log) => {
+                let pair_address = log.address;
 
-            let sender_address = Address::from(log.topics[1]);
-
-            let amount_0 = U256::from_big_endian(&log.data[0..32]);
-            let amount_1 = U256::from_big_endian(&log.data[32..64]);
-
-            println!("    sender: {}\n    amount_0: {}\n    amount_1: {}", sender_address, amount_0, amount_1);
-
-            // if pair_address_set.remove(&pair_address) {
-            //     let sender_address = Address::from(log.topics[1]);
-
-            //     let amount_0 = U256::from_big_endian(&log.data[0..32]);
-            //     let amount_1 = U256::from_big_endian(&log.data[32..64]);
-
-            //     println!("    sender: {}\n    amount_0: {}\n    amount_1: {}", sender_address, amount_0, amount_1);
-
-
-            //     // Buy if it meets liquidity criteria!
-            // }
-        }
+                // // TODO: Check the pool for the coins
+                // // One token *will* be WDMT due to pair creation filtering
+                // // Check which one is WDMT and for the one that isn't.
+                if pair_address_set.remove(&pair_address) {
+                    let sender_address = Address::from(log.topics[1]);
     
+                    let amount_0 = U256::from_big_endian(&log.data[0..32]);
+                    let amount_1 = U256::from_big_endian(&log.data[32..64]);
+    
+                    println!("    sender: {}\n    amount_0: {}\n    amount_1: {}", sender_address, amount_0, amount_1);
+                    // Buy if it meets liquidity criteria!
 
+                }
+            }
+        }
     }
-
-
-
-
-    // let swap_filter = Filter::new()
-    //     .event("Swap(address,uint256,uint256,uint256,uint256,address)");
-    //     // .topic1(token_topics.to_vec())
-    //     // .topic2(token_topics.to_vec());
-
-    // let mut swap_stream = provider.subscribe_logs(&swap_filter).await?;
-    // while let Some(log) = swap_stream.next().await {
-    //     println!("{:#?}", log);
-    // }
 
     Ok(())
 }
